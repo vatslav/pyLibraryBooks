1afa4e69af34594e1d1e5b2aafd2bf204713e3ca
vatslav-xenia-pk <vatslav.spam@yandex.ru>
2012-07-04 14:02:03 +0300

    запаздалый коммит
    проект разбит на ttk & tkinter
    работа над пас/логиншаг 2

---
+++ main on ttk.py
@@ -0,0 +1,67 @@
+#__author__ = 'Вячеслав'
+#коннект в бд
+#выбор типа пользователя
+#работа в нем
+from tkinter import *
+from tkinter.messagebox import *
+from tkinter.ttk import *
+from sys import exit as ext
+import sqlite3
+root = Tk()
+err = Label(root,text='')
+b1  = '<Button-1>'
+b1w = '<Double-1>'
+b2  = '<Button-2>'
+b2w = '<Double-2>'
+
+try:
+    conn = sqlite3.connect('library.db')
+    cur = conn.cursor()
+except:
+    err['text'] = 'ошибка открытия базы данных'
+    err.pack()
+    err.mainloop()
+    exit()
+#cur.execute("")
+#391 все из букс
+for row in cur.execute("select * from books"):
+    print(row)
+
+def okAct(event):
+    pass
+
+def quit(event):
+    ext()
+    return
+
+#GUI:
+logW = Entry(root)
+pasW = Entry(root)
+okW = Button(root, text='Ok')
+cancelW = Button(root, text='Cancel')
+logL = Label(root, text='Логин')
+pasL = Label(root, text='Пароль')
+
+
+
+logL.pack(side=LEFT)
+pasL.pack(side=LEFT)
+logW.pack(side=RIGHT)
+pasW.pack(side=RIGHT)
+okW.pack(side=LEFT)
+cancelW.pack(side=RIGHT)
+okW.bind(b1,okAct)
+okW.bind(b1w,quit)
+
+#logL.drid(row=0,column=0)
+#pasL.drid(row=1,column=0)
+#logW.drid(row=0,column=1)
+#pasW.drid(row=1,column=1)
+#okW.drid(row=2,column=0)
+#cancelW.drid(row=2,column=2)
+okW.bind(b1,okAct)
+okW.bind(b1w,quit)
+
+
+root.mainloop()
+print('finish')
