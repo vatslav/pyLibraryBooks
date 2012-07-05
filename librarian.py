36d07eb7e0e3278d267741c7f76c0c01eebb3462
vatslav-xenia-pk <vatslav.spam@yandex.ru>
2012-07-05 11:18:40 +0300

    нормальное окно для таблицы
    нужно еще реализовать название столбцов

--- librarian.py
+++ librarian.py
@@ -29,28 +29,40 @@ if __name__ == '__main__':
     tab.cells[2][2].value.set( tab.cells[1][1].value.get() )
 
     root.mainloop()'''
-def viewTable(x,y):
-    root = Tk()
+def viewTable(x,y,r1,title=1):
+    root = Toplevel(r1)
     tab = Table(root,x,y)
+    tab.pack()
+    tab.cells[1][1].value.set('test')
+    tab.cells[2][2].value.set( tab.cells[1][1].value.get() )
+    #root.mainloop()
+
 
 #def getinfo():
 
-def cratuser():
 
-root1=Tk()
-viewAllBooks   = Button (text='посмотреть список книг')
-creatLibrarian = Button (text='Создать библиотекаря')
-viewLibrarian  = Button (text='Посмотреть информацию о библиотекаре')
-#viewAllBooks.bind(b1,)
+def mymain():
+    #def cratuser():
 
+    root1=Tk()
+    viewAllBooks   = Button (text='посмотреть список книг')
+    creatLibrarian = Button (text='Создать библиотекаря')
+    viewLibrarian  = Button (text='Посмотреть информацию о библиотекаре')
 
-#viewAllBooks.pack(expand=False)
-#creatLibrarian.pack(expand=True)
-#viewLibrarian.pack()
-viewAllBooks.grid(padx=20,ipady=5)
-creatLibrarian.grid(columnspan=5,padx=20,ipady=5)
-viewLibrarian.grid(ipady=2)
+    #viewAllBooks.bind(b1,)
 
 
+    #viewAllBooks.pack(expand=False)
+    #creatLibrarian.pack(expand=True)
+    #viewLibrarian.pack()
+    viewAllBooks.grid(padx=20,ipady=5)
+    creatLibrarian.grid(columnspan=5,padx=20,ipady=5)
+    viewLibrarian.grid(ipady=2)
+    viewTable(5,5,root1)
 
-root1.mainloop()
+
+    root1.mainloop()
+
+
+
+mymain()
